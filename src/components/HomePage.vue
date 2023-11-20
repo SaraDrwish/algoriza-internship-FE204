0d12d9252109a4f249aa2706065d2689403a6d0b
Sara Drwish <38770404+SaraDrwish@users.noreply.github.com>
2023-11-20 13:58:28 +0300

    9 homepage 2

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,14 @@
 <template>
   <div class=" mt-[80px] mx-auto">
-    <div class=" flex fixed  left-[100px] w-[1240px] h-[500px] flex-shrink-0 ">
-       <div class=" relative      "> 
-        <div class=" overlay-home bg-black h-full w-full absolute opacity-40  ">  </div>
-          <img class="  rounded-[10px]" src="../assets/imgs/mainPAgeHomeImg.jpeg" alt="home main image">
-        </div>
-        <div class=" absolute left-[20%] top-[50%]   z-100 text-center ">
-          <h3>Enjoy Your Dream Vacation</h3>
-          <p>Plan and book our perfect trip with expert advice, travel tips, destination information and  inspiration from us</p>
+    <div class=" flex fixed  left-[100px]  w-[calc(100%-200px)] h-[500px] flex-shrink-0 ">
+      <!-- <div class=" flex fixed  left-[100px] w-[1240px] w-[calc(100%+2rem)] h-[500px] flex-shrink-0 "> -->
+       <div class=" relative h-full w-full  "> 
+          <div class="overlay-home rounded-[10px] overflow-hidden bg-black h-full w-full absolute opacity-40 "> </div> 
+          <img class="rounded-[10px] h-full w-full " src="../assets/imgs/mainPAgeHomeImg.jpeg" alt="home main image">
+        </div> 
+        <div class=" absolute left-[25%] top-[50%] w-[50%] m-auto z-100 text-center text-white  ">
+          <h3 class="text-[32px] font-bold">Enjoy Your Dream Vacation</h3>
+          <p class="text-[20px] line-h-28 ">Plan and book our perfect trip with expert advice, travel tips, destination information and  inspiration from us</p>
         </div>
     </div>
      
@@ -20,17 +21,5 @@
 </script>
 
 <style  scoped>
-img{
-/* width: 1240px;
- height: 500px;
-  flex-shrink: 0; */
-  /* background: linear-gradient(0deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.30) 100%), url(<path-to-image>), lightgray 0px -72.075px / 100% 165.959% no-repeat; */
-}
-
-.overlay-home{
-  /* width: 100%;
-  height: 100%; */
-  /* background-color: rgba(0, 0, 0, 0.7); */
-  /* background: linear-gradient(0deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.30) 100%), url(<path-to-image>), lightgray 0px -72.075px / 100% 165.959% no-repeat; */
-}
+ 
 </style>
