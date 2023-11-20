ccf3523ddfe1c703c332c04f3d65e333b70b9590
Sara Drwish <38770404+SaraDrwish@users.noreply.github.com>
2023-11-20 23:30:00 +0300

    11 homepage 4 diry

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 import Nav from "./components/Nav.vue"
-import HomaPage from "./components/HomePage.vue"
+import HomaPage from "./components/HomePageCompos/HomePage.vue"
   
 </script>
 
--- src/components/HomePageCompos/HomePage.vue
+++ src/components/HomePageCompos/HomePage.vue
@@ -6,7 +6,7 @@
       <div class=" flex fixed left-[100px] w-[calc(100%-200px)] h-[500px] flex-shrink-0 ">
           <div class="  h-full w-full  "> 
             <div class="overlay-home rounded-[10px] overflow-hidden bg-black opacity-30  h-full w-full absolute "> </div> 
-            <img class="rounded-[10px] h-full w-full " src="../assets/imgs/mainPAgeHomeImg.jpeg" alt="home main image">
+            <img class="rounded-[10px] h-full w-full " src="../../assets/imgs/mainPAgeHomeImg.jpeg" alt="home main image">
           </div> 
           <div class=" absolute left-[25%] top-[50%] w-[50%] m-auto z-100 text-center text-white  ">
             <h3 class="text-[32px] font-bold">Enjoy Your Dream Vacation</h3>
@@ -20,27 +20,27 @@
       <div class=" fixed left-[205px] bottom-[15%] w-[calc(100%-410px)] h-[64px] bg-red-200 p-4 ">
         <form action="#" class=" flex rounded-[8px]  gap-[15px] bg-red-500 ">
           <div class="flex gap-[10px]   bg-green-200 rounded-[4px] pt-[11px] pb-[12px] px-[12px]">
-            <img src="../assets/icons/location 1.svg" alt="location-icon">
+            <img src="../../assets/icons/location 1.svg" alt="location-icon">
             <input class="text-[13px]" type="text" placeholder="Where are you going?">
           </div>
 
           <div class="flex gap-[10px]   bg-green-200 rounded-[4px] pt-[11px] pb-[12px] px-[12px]">
-            <img src="../assets/icons/calendar 1.svg" alt="calendar-icon">
+            <img src="../../assets/icons/calendar 1.svg" alt="calendar-icon">
             <input class="text-[13px] " type="text" placeholder="Check in date">
           </div>
 
           <div class="flex gap-[10px]   bg-green-200 rounded-[4px] pt-[11px] pb-[12px] px-[12px]">
-            <img src="../assets/icons/calendar 1.svg" alt="calendar-icon">
+            <img src="../../assets/icons/calendar 1.svg" alt="calendar-icon">
             <input class="text-[13px]" type="text" placeholder="Check out date">
           </div>
 
           <div class="flex gap-[10px]   bg-green-200 rounded-[4px] pt-[11px] pb-[12px] px-[12px]">
-            <img src="../assets/icons/user-square 1.svg" alt="user-icon">
+            <img src="../../assets/icons/user-square 1.svg" alt="user-icon">
             <input class="text-[13px]" type="text" placeholder="Guests">
           </div>
 
           <div class="flex gap-[10px]   bg-green-200 rounded-[4px] pt-[11px] pb-[12px] px-[12px]">
-            <img src="../assets/icons/single_bed_FILL0_wght400_GRAD0_opsz24 1.svg" alt="single_bed">
+            <img src="../../assets/icons/single_bed_FILL0_wght400_GRAD0_opsz24 1.svg" alt="single_bed">
             <input class="text-[13px]" type="text" placeholder="Rooms">
           </div>
 
@@ -56,7 +56,7 @@
 
     <div class="flex items-center rounded-[8px] fixed bottom-4 left-[100px] h-[72px] py-[20px] pl-[24px] gap-[19px] w-[calc(100%-200px)] bg-yellow-100 ">
       <div class="flex items-center py-[5px]   ">
-        <img class="w-[32px] h-[32px] " src="../assets/icons/danger 1.svg" alt="danger">
+        <img class="w-[32px] h-[32px] " src="../../assets/icons/danger 1.svg" alt="danger">
       </div>
       <p>Check the latest COVID-19 restrictions before you travel .  <a href="#" class="text-primary"> Learn more </a> </p>
     </div>
