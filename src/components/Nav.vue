6e6facd7a8f152f21056a0e87df9f76fa5fa1816
Sara Drwish <38770404+SaraDrwish@users.noreply.github.com>
2023-11-20 12:36:25 +0300

    7 nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
-     <header class="fixed top-0 w-[1140px] h-[68px]  ">
-      <nav class="  flex flex-col sm:flex-row items-center gap-1 ">
+     <header class="fixed top-0 left-[100px]">
+      <nav class="flex sm:flex-row items-center justify-between  ">
 
         <RouterLink :to="{name:'home'}">
           <div class="flex items-center ">
@@ -18,9 +18,11 @@
             <li>Contact</li>
           </ul>
         </div>
+
         <div class="">
           <button class=" bg-BlueColor text-BlueColor p-1 m-1">Register</button>
         </div>
+
       </nav>
     </header>
  </template>
@@ -31,22 +33,19 @@ import { RouterLink } from 'vue-router';
 
 <style scoped>
 
- header{
-  margin: auto;
-  text-align: center;
- }
-
   nav{
-  width: 1440px;
+  width: calc(100vw - 200px);
   height: 68px;
   flex-shrink: 0;
-  background: rgb(132, 100, 163);
+  /* background: rgb(132, 100, 163); */
+  }
+  * ,body{
+   font-family: SF Pro Display;
+   font-style: normal;
   }
   p{
     color: #1B1F2D;
-    font-family: SF Pro Display;
     font-size: 18px;
-    font-style: normal;
     font-weight: 500;
     line-height: normal;
     letter-spacing: 0.36px;
@@ -55,27 +54,22 @@ import { RouterLink } from 'vue-router';
     display: inline-flex;
     align-items: flex-start;
     gap: 48px;
-    background-color: antiquewhite;
-
-    color:   #333 ;
-    font-family: SF Pro Display;
+    /* background-color: antiquewhite; */
+    color:#333 ;
     font-size: 16px;
-    font-style: normal;
     font-weight: 400;
     line-height: normal;
     letter-spacing: 0.32px;
   }
   li{
-   background: rgb(120, 71, 71);
+   /* background: rgb(120, 71, 71); */
   }
   button{
     background-color: var(--primary);
     padding: 10px 18px;
     color: #FFF;
-    font-family: SF Pro Display;
-    font-size: 15px;
-    font-style: normal;
-    font-weight: 500;
+     font-size: 15px;
+     font-weight: 500;
     line-height: 20px; /* 133.333% */
     letter-spacing: 0.3px;
 }
