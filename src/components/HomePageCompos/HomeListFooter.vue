dbe8921cff1a33b0a1de69585ec3702c3ea0809a
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2023-11-23 14:13:08 +0300

    26  homepage 19

--- src/components/HomePageCompos/HomeListFooter.vue
+++ src/components/HomePageCompos/HomeListFooter.vue
@@ -3,9 +3,10 @@
 
     <div class="container">
 
-      <div class=" w-[20%]  mb-[60px]">
+      <div class="flex mb-[60px] gap-[90px]">
 
-        <div class="flex flex-col gap-[7.5px] ">
+      <!-- //////////////////// -->
+        <div class="flex flex-col gap-[7.5px] w-[20%]  ">
            <div class="flex gap-[4px]">
             <img class="w-[24px] h-[24px]" src="../../assets//icons/bxs-plane-alt 1.svg" alt="plan"> 
             <p class="text-[18px] ">my Dream Place</p>
@@ -17,56 +18,56 @@
 
         <!-- ////////////// -->
          
-        <div class="">
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+        <div class="flex flex-col gap-[20px]">
+          <span class="font-bold text-[16px] ">Company</span>
+            <ul class="flex flex-col text-[14px] gap-[10px]">
+              <a href=""> <li>About</li> </a>
+              <a href=""> <li>Jobs</li> </a>
+              <a href=""> <li>Newsroom</li> </a>
+              <a href=""> <li>Advertising</li> </a>
+              <a href=""> <li>Contact us</li> </a>
             </ul>
         </div>
 
           <!-- ////////////////// -->
-          <div class="">
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
+           <div class="flex flex-col gap-[20px]">
+            <span class="font-bold text-[16px] ">Explore</span>
+              <ul class="flex flex-col text-[14px] gap-[10px]">
+                  <li>Australia</li>  
+                  <li>New Zealand</li> 
+                  <li>United States America (USA)</li >
+                  <li>Greece</li> 
+                  <li>Maldives</li>
+                  <li>Singapore</li>
+                  <a href="#"><li>See more</li></a>
+              </ul>
           </div>
 
           <!-- //////////// -->
 
-          <div class="">
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
+           <div class="flex flex-col gap-[20px]">
+              <span class="font-bold text-[16px] ">Terms and Policies</span>
+                <ul class="flex flex-col text-[14px] gap-[10px]">
+                    <li>Privacy Policy</li>  
+                    <li>Terms of use</li> 
+                    <li>Acessibility</li>
+                    <li>Reward system policy</li> 
+                </ul>
+            </div>
+
           <!-- /////////////// -->
 
-          <div class="">
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
+          <div class="flex flex-col gap-[20px]">
+              <span class="font-bold text-[16px] ">Help</span>
+              <ul class="flex flex-col text-[14px] gap-[10px]">
+                 <li>Support</li>  
+                 <li>Cancel your bookings</li> 
+                 <li>Refund Policies</li>
+                 <li>International Travel Documents</li> 
+              </ul>
           </div>
 
     <!-- //////////////////// -->
-
 
       </div>
  
