b5f57a174c03e215e41478f3fc7db2187600c5e1
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2023-11-24 19:41:45 +0300

    41  signin and register 12

--- src/components/register.vue
+++ src/components/register.vue
@@ -14,36 +14,37 @@
         <form @submit.prevent="handleSubmit" class="flex flex-col gap-[20px] m-auto w-[401px] " >
 
             <div class="flex flex-col gap-[4px]">
-                <label class="text-dark-black font-[500]  text-[14px]" >Email address</label>
-                <input v-model="email" class="w-[400px] bg-inputsGray pt-[11px] pb-[12px] px-[12px]" type="email" name="email" placeholder="Email" required >
+                <label for="email" class="text-dark-black font-[500]  text-[14px]" >Email address</label>
+                <input id="email" v-model="email" class="w-[400px] bg-inputsGray pt-[11px] pb-[12px] px-[12px]" type="email" name="email" placeholder="Email" required >
             </div>
 
             <div class="flex flex-col gap-[4px]">
-              <label  class="text-dark-black font-[500]  text-[14px]" >Password</label>
+              <label  for="password" class="text-dark-black font-[500]  text-[14px]" >Password</label>
               <div class="flex justify-between rounded-[4px] gap-[12px] items-center w-[400px]
                 bg-inputsGray pt-[11px] pb-[12px] px-[12px]  ">
-                <input v-model="password" class="w-full "
+                <input id="password" v-model="password" class="w-full "
                   type="password" name="password" placeholder="Password" required >
                 <img class="w-[20px] h-[20px] cursor-pointer " src="../assets/icons/eye 1.svg" alt="password">
               </div>
             </div>
 
             <div class="flex flex-col gap-[4px]">
-              <label class="text-dark-black font-[500]  text-[14px]" >Confirm password</label>
+              <label for="repassword" class="text-dark-black font-[500]  text-[14px]" >Confirm password</label>
               <div class="flex justify-between rounded-[4px] gap-[12px] items-center w-[400px] bg-inputsGray pt-[11px] pb-[12px] px-[12px]">
-                <input v-model="repassword" class="w-full " type="password" name="repassword" placeholder="Confirm Password" required >
+                <input id="repassword" v-model="repassword" class="w-full " type="password" name="repassword" placeholder="Confirm Password" required >
                 <img class="w-[20px] h-[20px] cursor-pointer " src="../assets/icons/eye 1.svg" alt="password">
               </div>
             </div>
 
-            <input   class="bg-primary text-white rounded-[6px] 
+            <input  class="bg-primary text-white rounded-[6px] 
              w-[100%] pt-[11px] pb-[12px] cursor-pointer mt-[33px]  " 
              type="submit" value="Submit" >
 
            </form>
 
            <div class="text-center mt-[20px] text-[16px] ">
-             <p>Already have an account? <a class="text-primary" href="/signin"> Sign in </a> </p>
+             <p>Already have an account? <router-link class="text-primary" to="/signin"> Sign in </router-link> </p>
+               <!-- <p>Already have an account? <a class="text-primary" href="/signin"> Sign in </a> </p> -->
            </div>
           
       </div>
@@ -67,18 +68,20 @@ import footerlastbtm from './footerlastbtm.vue';
 import { ref } from 'vue';
 import axios from 'axios';
 // import './axios';
-import { authService } from '@/services/authService';
+// import { authService } from '@/services/authService';
+import { useAuth0 } from '@auth0/auth0-vue'
+
 
 ////////////////////////////////////////////////////////////
 
 // const newForm = ref()
 
 //  const email = ref(email)
+
    const email = ref('')
    const password = ref('')
    const repassword = ref('') 
    
-
 // const handleRegister = async() => {
   
 //   console.log("registered", email.value, password, repassword);
@@ -96,34 +99,32 @@ import { authService } from '@/services/authService';
 //   $router.push("/login")
 // }
 
-
 const handleSubmit = async () => {
 
-
-  if(password !== repassword) {
+  if( password.value !==  repassword.value ) {
     alert("the password is not matching ");
     return;
   }
   try {
-    await authService.handleSubmit(email, password);
+    // await handleSubmit(email, password );
+    window.localStorage.setItem('emailReg' , email)
+    window.localStorage.setItem('passReg', password)
+    // await useAuth0.handleSubmit(email, password);
     console.log("you did registered successfuly ");
+    // alert("you did registered successfuly")
 
     // to clear the form :
-
     email.value = ""
     password.value = ""
     repassword.value = ""
 
   }
   catch (e){
-    console.log("registered failed  :( ", e)
+    console.log("registered failed  ", e)
   }
   
- 
 
-  
 }
-
 
 
 </script>
--- src/components/signIn.vue
+++ src/components/signIn.vue
@@ -8,34 +8,34 @@
 
       <div class="flex flex-col items-center m-auto w-[401px]  mt-[92px] ">
         <h2 class="text-[28px] mb-[40px]  font-[600] " >Sign In</h2>
-        <form class="flex flex-col gap-[20px] m-auto w-[401px] ">
+        <form @submit.prevent="handleSubmit" class="flex flex-col gap-[20px] m-auto w-[401px] ">
 
             <div class="flex flex-col gap-[4px] rounded-[4px]">
-              <label class="text-dark-black font-[500] text-[14px]" >Email address</label>
-              <input class=" w-[400px] bg-inputsGray pt-[11px] pb-[12px] px-[12px] "
+              <label for="emailIn" class="text-dark-black font-[500] text-[14px]" >Email address</label>
+              <input v-model="emailIn" id="emailIn" class=" w-[400px] bg-inputsGray pt-[11px] pb-[12px] px-[12px] "
                type="email" name="email" placeholder="Email" required >
             </div>
             
               <div class="flex flex-col gap-[4px] rounded-[4px]">
 
-                <label  class="text-[14px] text-dark-black font-[500]  " >Password</label>
+                <label for="passIn" class="text-[14px] text-dark-black font-[500]  " >Password</label>
 
                 <div class="flex justify-between rounded-[4px] gap-[12px] items-center w-[400px]
                     bg-inputsGray pt-[11px] pb-[12px] px-[12px]  ">
-                  <input class=" "
+                  <input v-model="passwordIn" id="passwordIn" 
                    type="password" name="password" placeholder="Password" required >
                   <img class="w-[20px] h-[20px] cursor-pointer " src="../assets/icons/eye 1.svg" alt="password">
                 </div>
 
               </div>
-
-              <input @click="handleLogin" class="bg-primary text-white rounded-[6px] mt-[33px] w-[100%] pt-[11px] pb-[12px] cursor-pointer " 
+   <!-- @click="handleLogin"   -->
+              <input  class="bg-primary text-white rounded-[6px] mt-[33px] w-[100%] pt-[11px] pb-[12px] cursor-pointer " 
                type="submit" value="Sign in" >
 
         </form>
 
         <div class="mt-[20px] text-[16px] ">
-          <p>Don’t have an account? <a class="text-primary" href="/register" > Register </a> </p>
+          <p>Don’t have an account? <router-link class="text-primary" to="/register">Register </router-link> </p>
         </div>
       
       </div>
@@ -54,18 +54,26 @@
 import NavSecndry from './Navs/NavSecndry.vue';
 //import NavSignined from './NavSignined.vue';
 import footerlastbtm from './footerlastbtm.vue';
-
+import { ref } from 'vue';
 // ////////////////////////////////////////////////////////////
 
 // import {useAuth0 }  from '@auth0/auth0-vue'
 // const {loginauth} = useAuth0()
 
-// const handleLogin = () => {
-//   loginauth({
-//     appState: {
-//       target: "/signedhome",
-//     },
-//   });
-// };
+const emailIn = ref('');
+const passwordIn = ref('')
+
+const handleSubmit = async () => {
+
+ const emailReg = window.localStorage.getItem('email')
+  const passReg = window.localStorage.getItem('password')
+
+  if (emailIn === emailReg && passwordIn === passReg) {
+    console.log("logged successfuly ")
+  } else {
+    console.error("faild login")
+  }
+  
+}
 
 </script>
