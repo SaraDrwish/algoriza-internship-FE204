62a4ab5aa1898081799a0297a928278e0d3a9036
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2023-12-01 15:30:04 +0300

    94  search and res / PD  19

--- src/components/ProdDetailscompo/ProductDetail.vue
+++ src/components/ProdDetailscompo/ProductDetail.vue
@@ -48,7 +48,7 @@
 
       <div class="bg-pink-900 flex flex-col p-2 mb-[172px] gap-[30px]  ">
         <!-- <div class="bg-bggraynstedpage"> -->
-        <div class="bg-yellow-500 p-2 relative top-[unset] bottom-0 m-auto   ">
+        <div class="bg-yellow-500 p-2   ">
           <!-- start products details nav  -->
           <div class="flex gap-[14px] text-[16px] font-[400]">
             <span class=" py-[10px] px-[14px] text-Gray2  border-b-4 border-primary  ">Overview</span>
@@ -59,7 +59,7 @@
 
           <div class="bg-pink-300 p-2 flex flex-col gap-[12px] flex-2">
           <p class="text-[32px] font-[600] text-checkoutText1 ">Lakeside Motel Warefront</p>
-              <!-- /////// -->
+            <!-- /////// -->
             <div class="flex gap-[12px] mb-[17px]">
               <span class="flex"> 
                 <img src="../../assets/icons/star-s-fill 4fl.svg" alt="str">
@@ -70,15 +70,70 @@
               </span>
               <span class="text-[14px] text-light-black"> 4.5 (1200 Reviews)</span>
             </div>
-            <div class="flex gap-[6px]  ">
+            <div class="flex gap-[6px] mb-[31px] ">
               <img class="w-[20px] h-[20px] " src="../../assets/icons/location2.svg" alt="locationPD">
               <span class="text-[14px] text-Gray2 ">Lorem ipsum road, Tantruim-2322, Melbourne, Australia</span>
             </div>
             <!-- /////// -->
 
-            <div class="bg-black-500">
+            <div class="bg-black-500  rounded-[6px] bg-white w-[818px]">
 
-              <p>Overview</p>
+              <p class="pt-[24px] pl-[30px] mb-[20px] text-[18px] text-checkoutText1 font-[500] " >Overview</p>
+              
+              <div class=" pl-[30px] mb-[44px]  mr-[40px] text-[16px]  text-light-black ">
+                <p>Featuring free WiFi throughout the property, Lakeside Motel Waterfront offers accommodations in Lakes Entrance, 19 mi from Bairnsdale. Free private parking is available on site.
+                </p>
+                <br>
+                <p>Each room at this motel is air conditioned and comes with a flat-screen TV. You will find a kettle, toaster and a microwave in the room. Each room is fitted with a private bathroom. Guests have access to barbecue facilities and a lovely large lawn area. Metung is 6.8 mi from Lakeside Motel Waterfront, while Paynesville is 14 mi from the property.
+                </p>
+                <br>
+                <p>Couples in particular like the location – they rated it 9.2 for a two-person trip.</p>
+              </div>
+
+              <hr class="fill-borderserbox text-borderserbox border-borderserbox">
+              <!-- <div class="w-[100%]">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="2" viewBox="0 0 810 2" fill="none">
+                  <path d="M0 1H810" stroke="#E0E0E0"/>
+                </svg>
+              </div> -->
+              
+
+              <div class="mt-[26px] pl-[30px] pb-[40px] ">
+                <p class="mb-[24px] text-[18px] font-[500] text-checkoutText1  ">Top facilities</p>
+                <ul class="flex text-[15px]  gap-[100px]  text-light-black " >
+
+                  <div class="flex flex-col gap-[13px]">
+                    <li class="flex gap-[12px]" >
+                      <img src="../../assets/icons/home-wifi 1.svg" alt="wifi">
+                      <span>Free wifi</span>
+                    </li>
+                    <li class="flex gap-[12px]">
+                      <img src="../../assets/icons/wind 1.svg" alt="wind">
+                      <span>Air Conditioning</span>
+                    </li>
+                    <li class="flex gap-[12px]" >
+                      <img src="../../assets/icons/car 1.svg" alt="car">
+                      <span>Parking available</span>
+                    </li>
+                  </div>
+
+                  <div class="flex flex-col gap-[13px]">
+                    <li class="flex gap-[12px]" >
+                      <img src="../../assets/icons/bag-tick-2 1.svg" alt="bag">
+                      <span>Business Services</span>
+                    </li>
+                    <li class="flex gap-[12px]" >
+                      <img src="../../assets/icons/lifebuoy 1.svg" alt="lifebuoy">
+                      <span>Swimming pool</span>
+                    </li>
+                    <li class="flex gap-[12px]" >
+                      <img src="../../assets/icons/like-1 1.svg" alt="like">
+                      <span>Top rated in area</span>
+                    </li>
+                  </div>
+                  
+                </ul>
+              </div>
               
             </div>
 
@@ -88,14 +143,14 @@
 
           <div class="flex flex-col flex-1 bg-green-100 p-2 gap-[35px] ">
 
-            <div class="">
-              <img class=" rounded-[6px]" src="../../assets/imgs/locationmap1.png" alt="locationmap1">
+            <div class=" bg-green-900 p-2 ">
+              <img class=" w-[400px] h-[240px]  rounded-[6px]" src="../../assets/imgs/locationmap1.png" alt="locationmap1">
             </div>
-            <div class="  bg-green-300 p-2 gap-[22px]">
-              <p class="text-[18px] font-[500] ">Explore the area</p>
-              <ul class="flex flex-col justify-between bg-blue-200 p-2" >
-                <li class="flex bg-orange-100 p-2">
-                  <div class="flex text-left items-start">
+            <div class=" bg-green-400 p-2 gap-[22px] ">
+              <p class="text-[18px] font-[500] pb-[22px] ">Explore the area</p>
+              <ul class="flex flex-col justify-between bg-blue-200 p-2 text-[14px] gap-[12px] " >
+                <li class="flex justify-between ">
+                  <div class="flex gap-[8px]" >
                       <img  src="../../assets/icons/bxs-plane-alt 2.svg" alt="bxs-plane-alt 2.svg">
                       <span>Hotel Penselvenyia</span>
                   </div>
@@ -103,20 +158,46 @@
                     <span>2 min drive</span>
                   </div>
                 </li>
-                <li class="flex bg-orange-200 p-2" >
-                  <div class="flex">
+                <li class="flex justify-between " >
+                  <div class="flex gap-[8px]">
                       <img src="../../assets/icons/bxs-map 1.svg" alt="locationPD2">
                       <span>Travis Bakery store house</span>
                   </div>
-                  <div class="">
-                    <span>2 min drive</span>
+                  <div >
+                    <span>10 min drive</span>
+                  </div>
+                </li>
+                <li class="flex justify-between " >
+                  <div class="flex gap-[8px]">
+                      <img src="../../assets/icons/bxs-map 1.svg" alt="locationPD2">
+                      <span>Olivia Johnson Garden</span>
+                  </div>
+                  <div >
+                    <span>15 min drive</span>
+                  </div>
+                </li>
+                  <li class="flex justify-between " >
+                  <div class="flex gap-[8px]">
+                      <img src="../../assets/icons/bxs-map 1.svg" alt="locationPD2">
+                      <span>Norman Opera Circus</span>
+                  </div>
+                  <div >
+                    <span>18 min drive</span>
+                  </div>
+                </li>
+                  <li class="flex justify-between " >
+                  <div class="flex gap-[8px]">
+                      <img src="../../assets/icons/bxs-map 1.svg" alt="locationPD2">
+                      <span>Rockdeset hotel</span>
+                  </div>
+                  <div >
+                    <span>32 min drive</span>
                   </div>
                 </li>
               </ul>
             </div>
           </div>
 
-  
 
         </div>
 
@@ -136,14 +217,11 @@
 
         </div>
 
-
         <!-- /// -->
-
 
       </div>
 
     </div>
-
 
 
       <!-- /////////////////// -->
@@ -152,7 +230,6 @@
       <div class="top-[unset] h-[72px]  w-full mb-[70px] ">
         <HomeAlert class=" top-[unset]  "  />
       </div>
-      <!-- <div class="mb-[unset] pb-[60px]" > -->
         <div class=" " >
         <HomeListFooter   />
       </div>
