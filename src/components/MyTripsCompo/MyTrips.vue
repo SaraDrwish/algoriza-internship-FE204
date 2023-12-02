dd655f5ffd509ea3836139647cda0b2fd7910cf2
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2023-12-03 01:56:12 +0300

    121 checout  46

--- src/components/MyTripsCompo/MyTrips.vue
+++ src/components/MyTripsCompo/MyTrips.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class=" min-h-[100vh] max-w-[100vw] relative bg-green-300 flex items-center justify-between mx-auto">
+  <div class=" mt-[128px] bg-green-300 relative  ">
 
     <div class="">
       <NavSignined/>
     </div>
 
 <!-- start container -->
-    <!-- left-1/2 transform -translate-x-1/2 -->
-  <div class="bg-pink-600 p-2 text-center w-full max-w-[calc(100%-200px)]
-      flex flex-col mx-auto absolute 
-      
-        " >
+ 
+  <div class="bg-pink-600 p-2 text-center  m-auto w-[calc(100%-100px)] " >
 
       <p class="text-[32px] font-[600] text-checkoutText1">My trips</p>
       
-      <div class="bg-orange-500 p-4  w-[calc(100%-200px)] absolute bottom-0
-            left-1/2 transform -translate-x-1/2  ">
+      <div class="bg-orange-500 p-2 ">
 
         <!-- //start small divs  -->
 
@@ -64,30 +60,30 @@
 
         </div>
 
-    </div>
 
         <!-- //end small divs  -->
-    
-    </div>
-
-  </div>
-
-<!-- end the container -->
   
+        <!-- start footer -->
 
-    <!-- start footer -->
+          <div class="relative w-full  ">
+            <HomeAlert class="top-[unset] "/>
+          </div>
 
-    <div class="bg-slate-600 p-2  w-[calc(100%-200px)] absolute bottom-0
-      left-1/2 transform -translate-x-1/2 " >
-      <HomeAlert/>
-      <HomeListFooter/>
+          <div class="bg-slate-600 p-2  bottom-0  " >
+            <HomeListFooter/>
+          </div>
+          
+        <!-- end footer -->
+        </div>
+
+      </div>
     </div>
 
-    <div class=" w-full absolute left-0 bottom-0 overflow-hidden ">
-      <footerlastbtmVue/>
-    </div>
+  <!-- end the container -->
 
-    <!-- end footer -->
+    <div class=" ">
+      <footerlastbtmVue class="absolute bottom-0 w-full"/>
+    </div>
 
   </div>
 </template>
@@ -100,7 +96,3 @@ import HomeListFooter from '../HomePageCompos/HomeListFooter.vue';
 import HomeAlert from '../HomePageCompos/HomeAlert.vue';
 
 </script>
-
-<style  scoped>
-
-</style>
