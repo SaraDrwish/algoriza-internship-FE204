f1a3cf0a768972b993c64895496897a682a81be4
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-01-18 09:01:35 +0300

    Update publicPath for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "https://SaraDrwish.github.io/algoriza-internship-FE204",
+  // publicPath: "https://SaraDrwish.github.io/alg/vue-deploy/oriza-internship-FE204",
+  // publicPath: "/vue-deploy/",
+  publicPath: "./",
   base: "/deploying-vite-algoriza-internship-FE204/",
   plugins: [vue()],
   resolve: {
